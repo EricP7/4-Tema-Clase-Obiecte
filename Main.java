7eaf627bf25f9bf3a1fc8a111be58a72b7117063
eric <ericpredit@gmail.com>
2025-03-14 12:14:39 +0200

    Main.java class added

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sertar sertar1 = new Sertar(30, 50, 40);
+		
+		System.out.println(sertar1.tipareste());
+
+	}
+
+}
