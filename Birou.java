f0598b0a43627a76ed99c27b11d84db09523d1c7
eric <ericpredit@gmail.com>
2025-03-14 12:30:42 +0200

    new class

---
+++ Birou.java
@@ -0,0 +1,15 @@
+
+public class Birou {
+	private Sertar sertar1, sertar2;
+	private int l, L, H;
+	
+	public Birou(int l, int L, int H, Sertar sertar1, Sertar sertar2) {
+		this.l = l;
+		this.L = L;
+		this.H = H;
+		this.sertar1 = sertar1;
+		this.sertar2 = sertar2;
+	}
+	
+	
+}
