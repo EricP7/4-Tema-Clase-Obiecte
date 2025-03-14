71cb3ed9cdcae11e77dd2159072b014cbf9b513b
eric <ericpredit@gmail.com>
2025-03-14 12:11:19 +0200

    constructor and tipareste method created for Sertar.java class

--- Sertar.java
+++ Sertar.java
@@ -2,4 +2,13 @@
 public class Sertar {
 	private int l, L, H;
 	
+	public Sertar(int l, int L, int H) {
+		this.l = l;
+		this.L = L;
+		this.H = H;
+	}
+	
+	public String tipareste() {
+		return "Sertar" + l + L + H;
+	}
 }
